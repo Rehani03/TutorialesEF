d313b6e6c6f86efac9efacb8709f42fcb53b9cc5
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-04 16:51:38 -0400

    Primeros ejemplos..

---
+++ TutorialesEF/DAL/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TutorialesEF.Entidades;
+
+namespace TutorialesEF.DAL
+{
+    public class SchoolContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database = SchoolDB; Trusted_Connection = True; ");
+        }
+    }
+}
--- TutorialesEF/Program.cs
+++ TutorialesEF/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using TutorialesEF.BLL;
+using TutorialesEF.Entidades;
 
 namespace TutorialesEF
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo!");
+            //Ejemplo de guardar en DB
+            GuardarDB();
         }
+
+        private static void GuardarDB()
+        {
+            //Ejemplo de guardar en DB
+            TutorialesEjemplosBLL tutoriales = new TutorialesEjemplosBLL();
+            var auxStudent = new Student()
+            {
+                StudentId = 0,
+                Name = "Bill"
+            };
+
+            if (tutoriales.EscribirDB(auxStudent))
+                Console.WriteLine("The Student was sucessfully saved!!");
+            else
+                Console.WriteLine("We cant save the student..");
+        }
+       
     }
 }
